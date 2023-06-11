dd644c428d4342bd29b1b1eb5f4a2db7bb494152
hufahamdika <hufahamdika7997@gmail.com>
2023-06-11 11:56:28 +0700

    added code

---
+++ runserver_process.sh
@@ -0,0 +1,9 @@
+#jalankan 5 server_process_pool
+
+python3 server_process_pool_http.py 8000 &
+python3 server_process_pool_http.py 8001 &
+python3 server_process_pool_http.py 8002 &
+python3 server_process_pool_http.py 8004 &
+
+
+
